eeb39d8c72ff5638962cc1460eea2c4b98cdc9fb
Kouji Tsuru <pafuxu@gmail.com>
2011-01-09 02:30:15 +0900

    ライトボックスを追加

--- global/js/augmo2011.js
+++ global/js/augmo2011.js
@@ -1,7 +1,19 @@
 // entry form lightbox
   $(function(){
     $("#entry-button").click(function() {
-      $("#entry-form").lightbox_me({centered: true});
+      $("#entry-form").lightbox_me({
+        centered: true,
+        overlaySpeed: "fast",
+        overlayDisappearSpeed: "fast"
+        });
+      return false;
+    });
+    $("#supporter-button").click(function() {
+      $("#supporter-list").lightbox_me({
+        centered: true,
+        overlaySpeed: "fast",
+        overlayDisappearSpeed: "fast"
+        });
       return false;
     });
   });
